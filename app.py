1614623ba3bfa0e285e45a9d3bf0fbf5cfe1669a
Darrin Nielson <31453605+darrin912@users.noreply.github.com>
2025-07-02 13:55:48 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, render_template_string
+from urllib.parse import quote
+
+app = Flask(__name__)
+
+# HTML template for the web form
+HTML_TEMPLATE = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Supply Side Connect New Jersey Schedule URL Creator</title>
+</head>
+<body>
+    <h1>Supply Side Connect New Jersey Schedule URL Creator</h1>
+    <p>Copy the Swapcard URL that you want to link to. If you have filters or searches, make sure you copy the entire URL. Paste that URL below:</p>
+    <form method="POST">
+        <label for="swapcard_url">Swapcard URL:</label><br>
+        <input type="text" id="swapcard_url" name="swapcard_url" style="width: 60%;" placeholder="Enter your Swapcard URL" required><br><br>
+        <button type="submit">Get New URL</button>
+    </form>
+    {% if new_url %}
+        <h3>Here is your link to the filtered iframe schedule. Make sure to test the link to make sure it works as expected.</h3>
+        <p>
+            <input type="text" value="{{ new_url }}" style="width: 60%;" readonly><br><br>
+            <button onclick="navigator.clipboard.writeText('{{ new_url }}')">Copy to Clipboard</button>
+        </p>
+    {% endif %}
+</body>
+</html>
+"""
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    new_url = None
+    if request.method == "POST":
+        old_url = request.form.get("swapcard_url")
+        # Process the URL
+        if "/planning" in old_url or "/plannings" in old_url:
+            split_url = old_url.split("/planning", 1)
+            remaining_url = "/planning" + split_url[1]
+            # Encode the remaining URL to escape special characters
+            encoded_remaining_url = quote(remaining_url)
+            new_url = f"https://newjersey.supplysideconnect.com/en/expo/event-information/schedule.html?page={encoded_remaining_url}"
+    return render_template_string(HTML_TEMPLATE, new_url=new_url)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
